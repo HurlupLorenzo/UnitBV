eb708b5328c682e76d2645267ad3e54db8e782e8
HurlupLorenzo <hurlup.lorenzo@gmail.com>
2019-04-01 10:41:21 +0300

    A step forward

--- MVP/TotalCommander/TotalCommander/MainWindow.xaml.cs
+++ MVP/TotalCommander/TotalCommander/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,33 @@ namespace TotalCommander
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Files
+    {
+        public Files()
+        {
+            
+        }
+        public void GettingFiles()
+        {
+            //creating a DirectoryInfo object
+            DirectoryInfo mydir = new DirectoryInfo(@"C:\");
+      
+
+            // getting the files in the directory, their names and size
+            FileInfo[] f = mydir.GetFiles();
+            
+        }
+        public void GettingFiles1()
+        {
+            //creating a DirectoryInfo object
+            
+            DirectoryInfo mydir1 = new DirectoryInfo(@"D:\");
+
+            // getting the files in the directory, their names and size
+            
+            FileInfo[] f1 = mydir1.GetFiles();
+        }
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -27,6 +55,16 @@ namespace TotalCommander
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
